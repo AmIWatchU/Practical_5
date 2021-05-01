1a2587d309543f50bd87544d808e433a73cf63b7
AmIWatchU <user106512@gmail.com>
2021-05-01 16:58:37 +0300

    Add project files.

---
+++ Ex 1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Ex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Buble sorting
+            Console.WriteLine("Сортування бульбашкою: ");
+
+            int[] arrayInt = { 13, 41, 94, 5, 12, 54, 66, 41, 95};           
+            for (int i = 0; i < arrayInt.Length-1; i++)
+            {
+                for (int j = i+1; j < arrayInt.Length; j++)
+                {
+                    if (arrayInt[i] > arrayInt[j])
+                    {
+                        int tmp = arrayInt[i];
+                        arrayInt[i] = arrayInt[j];
+                        arrayInt[j] = tmp;
+                    }
+                }
+            }
+            foreach (int a in arrayInt)
+            {
+                Console.Write($"{a} ");
+            }
+
+
+            //Parse sorting
+            Console.WriteLine("\n Сортування вставкою: ");
+            // int[] arrayInt = { 13, 41, 94, 5, 12, 54, 66, 41, 95};
+            int min;
+            int minI;            
+            for (int i = 0; i < arrayInt.Length-1; i++)
+            {
+                min = arrayInt[i];
+                minI = i;
+                for (int j = i+1; j < arrayInt.Length; j++)
+                {
+                    if (min > arrayInt[j])
+                    {
+                        minI = j;
+                        min = arrayInt[j];
+                    }
+                }
+                arrayInt[minI] = arrayInt[i];
+                arrayInt[i] = min;
+            }
+
+            foreach (int a in arrayInt)
+            {
+                Console.Write($"{a} ");
+            }
+
+            // Array.Sort();
+            Console.WriteLine("\n Сортування методом Array.Sort(): ");
+            // int[] arrayInt = { 13, 41, 94, 5, 12, 54, 66, 41, 95};
+
+            Array.Sort(arrayInt);
+            foreach (int a in arrayInt)
+            {
+                Console.Write($"{a} ");
+            }
+            
+            //Сортування string
+            string[] arrayStr = { "apple", "welcome", "gnome", "member", "spring", "book" };
+            Console.WriteLine("\n Сортування string методом Array.Sort(): ");
+            Array.Sort(arrayStr);
+            foreach (string a in arrayStr)
+            {
+                Console.Write($"{a} ");
+            }
+            //Інверування string
+            Console.WriteLine("\n Iнвертування string методом Array.Reverse(): ");
+            Array.Reverse(arrayStr);
+            foreach (string a in arrayStr)
+            {
+                Console.Write($"{a} ");
+            }
+
+            Console.ReadKey();
+        }        
+    }
+}
